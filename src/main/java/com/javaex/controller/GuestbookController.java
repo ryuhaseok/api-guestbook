f5330f5a67f7e6f55de4a718b11fb1da721b3db9
rhs0402 <rhs0402@naver.com>
2024-03-22 17:52:10 +0900

    1

--- src/main/java/com/javaex/controller/GuestbookController.java
+++ src/main/java/com/javaex/controller/GuestbookController.java
@@ -3,25 +3,25 @@ package com.javaex.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.GuestbookService;
 import com.javaex.vo.GuestbookVo;
 
-@Controller
+@RestController
 public class GuestbookController {
 	
 	@Autowired
 	private GuestbookService guestbookService;
 	
 	//삭제
-	@ResponseBody
-	@RequestMapping(value="/api/guests{no}", method= {RequestMethod.DELETE})
+	//@ResponseBody
+	@DeleteMapping(value="/api/guests{no}")
 	public int remove(@RequestBody GuestbookVo guestbookVo,
 						@PathVariable(value="no") int no) {
 		System.out.println("GuestbookController.remove()");
@@ -33,8 +33,8 @@ public class GuestbookController {
 	}
 	
 	//등록+1개 가져오기
-	@ResponseBody
-	@RequestMapping(value="/api/guests", method= {RequestMethod.POST})
+	//@ResponseBody
+	@PostMapping(value="/api/guests")
 	public GuestbookVo add(@RequestBody GuestbookVo guestbookVo) {
 		System.out.println("GuestbookController.add()");
 		System.out.println(guestbookVo);
@@ -47,8 +47,8 @@ public class GuestbookController {
 	
 
 	//리스트
-	@ResponseBody
-	@RequestMapping(value="/api/guests", method= {RequestMethod.GET})
+	//@ResponseBody
+	@GetMapping(value="/api/guests")
 	public List<GuestbookVo> list() {
 		System.out.println("GuestbookController.list()");
 		
